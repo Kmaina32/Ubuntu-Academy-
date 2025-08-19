1b20de45456099bdff1afdcb8d742656734e2235
kishan san <kishansan28@gmail.com>
2025-08-19 13:28:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  projectId: 'mkenya-skilled',
-  appId: '1:971829599240:web:743a5618282680bb9dc5bd',
-  storageBucket: 'mkenya-skilled.firebasestorage.app',
-  apiKey: 'AIzaSyBe1wU8Q-415auhlXf9k7ZXefvLX9TPOF0',
-  authDomain: 'mkenya-skilled.firebaseapp.com',
-  measurementId: '',
-  messagingSenderId: '971829599240',
-  databaseURL: 'https://mkenya-skilled-default-rtdb.firebaseio.com/',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
