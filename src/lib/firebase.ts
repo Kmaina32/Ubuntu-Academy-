dc3bc8f39d84ea7854e27f7bac772d12e5914a1a
Ubuntu Academy <kishansan28@gmail.com>
2025-09-24 07:06:05 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,14 +6,15 @@ import { getStorage } from 'firebase/storage';
 import { getRemoteConfig } from 'firebase/remote-config';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeiC1fLWb-zoh_lSS1j-qal8f3LX5VelM",
+  apiKey: "AIzaSyBe1wU8Q-415auhlXf9k7ZXefvLX9TPOF0",
   authDomain: "mkenya-skilled.firebaseapp.com",
   databaseURL: "https://mkenya-skilled-default-rtdb.firebaseio.com",
   projectId: "mkenya-skilled",
-  storageBucket: "mkenya-skilled.appspot.com",
+  storageBucket: "mkenya-skilled.firebasestorage.app",
   messagingSenderId: "971829599240",
-  appId: "1:971829599240:web:c7ae181183e385219dc5bd",
+  appId: "1:971829599240:web:743a5618282680bb9dc5bd"
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
