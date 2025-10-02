46002495814b5ee94395c4b34f2ba2ad75ac6441
Your Name <kishansan28@gmail.com>
2025-10-02 13:22:15 +0000

    what can be added to the hackathon sidebar?

--- src/components/PortalSidebar.tsx
+++ src/components/PortalSidebar.tsx
@@ -37,7 +37,7 @@ export function PortalSidebar() {
         <SidebarHeader className="mb-4">
             <div className="flex items-center gap-2">
                 <Gem className="h-6 w-6 text-primary" />
-                <span className="font-bold text-lg font-headline group-data-[collapsible=icon]:hidden">Akili A.I Academy</span>
+                <span className="font-bold text-lg font-headline group-data-[collapsible=icon]:hidden">Akili A.I. Academy</span>
             </div>
         </SidebarHeader>
         <SidebarContent>
